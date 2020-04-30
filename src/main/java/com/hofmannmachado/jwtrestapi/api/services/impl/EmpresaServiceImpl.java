85cc13e69712c78c691d906e6a60779498ff9f8d
Marcelo Hofmann Machado <marcelo.hofmann-machado@lhind.dlh.de>
2020-04-30 13:25:30 +0200

    add test for EmpresaServiceImpl

--- src/main/java/com/hofmannmachado/jwtrestapi/api/services/impl/EmpresaServiceImpl.java
+++ src/main/java/com/hofmannmachado/jwtrestapi/api/services/impl/EmpresaServiceImpl.java
@@ -6,9 +6,11 @@ import com.hofmannmachado.jwtrestapi.api.services.EmpresaService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
+@Service
 public class EmpresaServiceImpl implements EmpresaService {
 
     private static final Logger log = LoggerFactory.getLogger(EmpresaServiceImpl.class);
